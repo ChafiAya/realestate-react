8cda7da41037a56ecac542039a192b4cc37c7d75
unknown <chafi.1111.aya@gmail.com>
2023-04-02 07:31:39 +0200

    complete header component and make it dynamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import '../css/Header.css';
-import SignIn from "../pages/SignIn";
+import SignIn from '../pages/SignIn';
 
 export default function Header() {
+  const [pageState, setPageState] = useState('Sign in');
   const location = useLocation();
   const navigate = useNavigate();
+  const auth = getAuth();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setPageState('Profile');
+        setShowSignIn(false); // close the login panel if the user logs in successfully
+      } else {
+        setPageState('Sign in');
+      }
+    });
+  }, [auth]);
+
+  function pathMatchRoute(route) {
+    return route === location.pathname;
+  }
+
   const [showSignIn, setShowSignIn] = useState(false);
 
   const toggleSignIn = () => {
-    setShowSignIn(!showSignIn);
+    if (pageState === 'Sign in') {
+      setShowSignIn(!showSignIn);
+    } else {
+      setShowSignIn(false);
+      navigate('/profile');
+    }
   };
 
-  
- 
   const handleForgotPasswordClick = () => {
     setShowSignIn(false); // close the login panel
     navigate('/forgot-password');
@@ -23,7 +45,6 @@ export default function Header() {
     setShowSignIn(false); // close the login panel
     navigate('/register');
   };
-
 
   return (
     <div className="header">
@@ -41,7 +62,8 @@ export default function Header() {
               className={`header__list-item sign-in-button ${
                 location.pathname === '/' ? 'active' : ''
               }`}
-              onClick={() => navigate('/')}>
+              onClick={() => navigate('/')}
+            >
               <img
                 src="https://img.icons8.com/pulsar-line/512/home-page.png"
                 style={{
@@ -58,22 +80,28 @@ export default function Header() {
               className={`header__list-item sign-in-button ${
                 location.pathname === '/offers' ? 'active' : ''
               }`}
-              onClick={() => navigate('/offers')}>
+              onClick={() => navigate('/offers')}
+            >
               Offers
             </li>
             <li
               className={`header__list-item sign-in-button ${
                 location.pathname === '/about-us' ? 'active' : ''
               }`}
-              onClick={() => navigate('/about-us')}>
+              onClick={() => navigate('/about-us')}
+            >
               About us
             </li>
             <li
               className={`header__list-item sign-in-button ${
                 location.pathname === '/sign-in' ? 'active' : ''
+              } ${
+                (pathMatchRoute('/sign-in') || pathMatchRoute('/profile')) &&
+                'text-black border-b-red-500'
               }`}
-              onClick={toggleSignIn}>
-              Sign in
+              onClick={toggleSignIn}
+            >
+              {pageState}
             </li>
           </ul>
         </div>
@@ -81,12 +109,16 @@ export default function Header() {
       {showSignIn && (
         <div className="sign-in">
           <div className="sign-in2" onClick={(e) => e.stopPropagation()}>
-            <button className="close-button" onClick={toggleSignIn}> X </button>
-            <SignIn onForgotPasswordClick={handleForgotPasswordClick} onRegisterClick={handleRegisterClick} />
-           
+            <button className="close-button" onClick={toggleSignIn}>
+              X
+            </button>
+            <SignIn
+              onForgotPasswordClick={handleForgotPasswordClick}
+              onRegisterClick={handleRegisterClick}
+            />
           </div>
         </div>
       )}
     </div>
   );
-}
+      }
