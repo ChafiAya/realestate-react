9f7ffdd4f6944048952c1901a2b59e1550cb438f
unknown <chafi.1111.aya@gmail.com>
2023-04-08 18:27:43 +0200

    add my listings section to the profile page and create the listingItem component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -15,7 +15,7 @@ import { toast } from "react-toastify";
 import { db } from "../firebase";
 import { FcHome } from "react-icons/fc";
 import { useEffect } from "react";
-
+import ListingItem from "../components/ListingItem";
 
 export default function Profile() {
   const auth = getAuth();
@@ -79,19 +79,7 @@ export default function Profile() {
     }
     fetchUserListings();
   }, [auth.currentUser.uid]);
-  async function onDelete(listingID) {
-    if (window.confirm("Are you sure you want to delete?")) {
-      await deleteDoc(doc(db, "listings", listingID));
-      const updatedListings = listings.filter(
-        (listing) => listing.id !== listingID
-      );
-      setListings(updatedListings);
-      toast.success("Successfully deleted the listing");
-    }
-  }
-  function onEdit(listingID) {
-    navigate(`/edit-listing/${listingID}`);
-  }
+
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -156,7 +144,28 @@ export default function Profile() {
           </button>
         </div>
       </section>
-    
+
+      <div className="max-w-6xl px-3 mt-6 mx-auto">
+        {!loading && listings.length > 0 && (
+          <>
+            <h2 className="text-2xl text-center font-semibold mb-6">
+              My Listings
+            </h2>
+            <ul className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+              {listings.map((listing) => (
+                <ListingItem
+                key={listing.id}
+                id={listing.id}
+                listing={listing.data}
+               
+              />
+         
+                ))}
+                </ul>
+                </>
+        )}
+      </div>
+
     </>
-  )
+  );
 }
