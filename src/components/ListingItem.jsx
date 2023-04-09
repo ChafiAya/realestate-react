ace2d93d1cff6179cb345dad98f1ad4cd9f4a15f
unknown <chafi.1111.aya@gmail.com>
2023-04-09 19:08:44 +0200

    add share copy functionality

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,3 +1,4 @@
+
 import Moment from "react-moment";
 import { Link } from "react-router-dom";
 import { MdLocationOn } from "react-icons/md";
@@ -7,13 +8,15 @@ export default function ListingItem({ listing, id, onEdit, onDelete }) {
   return (
     <li className="relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150 m-[10px]">
       <Link className="contents" to={`/category/${listing.type}/${id}`}>
-        <img
-          className="h-[170px] w-full object-cover hover:scale-105 transition-scale duration-200 ease-in"
-          loading="lazy"
-          src={listing.imgUrls[0]}
-        />
+      <img
+         className="h-[170px] w-full object-cover hover:scale-105 transition-scale duration-200 ease-in"
+         loading="lazy"
+         src={listing.imgUrls[0]}
+         style={{ borderTopRightRadius: '50%', borderBottomLeftRadius: '50%' }}
+      />
+
         <Moment
-          className="absolute top-2 left-2 bg-[#3377cc] text-white uppercase text-xs font-semibold rounded-md px-2 py-1 shadow-lg"
+          className="absolute top-2 left-2 bg-[#98801f] text-white uppercase text-xs font-semibold rounded-md px-2 py-1 shadow-lg"
           fromNow
         >
           {listing.timestamp?.toDate()}
@@ -26,7 +29,7 @@ export default function ListingItem({ listing, id, onEdit, onDelete }) {
             </p>
           </div>
           <p className="font-semibold m-0 text-xl truncate">{listing.name}</p>
-          <p className="text-[#457b9d] mt-2 font-semibold">
+          <p className="text-[#98801f] mt-2 font-bold">
             $
             {listing.offer
               ? listing.discountedPrice
@@ -39,18 +42,19 @@ export default function ListingItem({ listing, id, onEdit, onDelete }) {
           </p>
           <div className="flex items-center mt-[10px] space-x-3">
             <div className="flex items-center space-x-1">
-              <p className="font-bold text-xs">
-                {listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
-              </p>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-green-600" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M16.5 16.5v-6h-1v6h-6v-1h5v-4h-4v-1h4.5v-4h-7v12h-2v-7h-2l-1.5 7h-1l1.5-7h-2v-1h2v-6h12v6h2v1h-2v4h5v1h-6zM4.5 9.5h11v-3h-11v3z" clipRule="evenodd" />
+                </svg>
+                <p className="font-bold text-xs">{listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}</p>
             </div>
             <div className="flex items-center space-x-1">
-              <p className="font-bold text-xs">
-                {listing.bathrooms > 1
-                  ? `${listing.bathrooms} Baths`
-                  : "1 Bath"}
-              </p>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-green-600" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M16.5 5.5v5h-1v-5h-5v-1h5v-4h-4v-1h4.5v6h-2v1h2.5zM3.5 9.5h11v-3h-11v3z" clipRule="evenodd" />
+                </svg>
+                <p className="font-bold text-xs">{listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath"}</p>
             </div>
-          </div>
+            </div>
+
         </div>
       </Link>
       {onDelete && (
@@ -68,5 +72,3 @@ export default function ListingItem({ listing, id, onEdit, onDelete }) {
     </li>
   );
 }
-
-    
--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -13,61 +13,114 @@ import SwiperCore, {
 } from "swiper";
 import "swiper/css/bundle";
 import {
-  FaShare,
   FaMapMarkerAlt,
   FaBed,
   FaBath,
   FaParking,
   FaChair,
+  FaShare,
+  FaFacebook,
+  FaWhatsapp,
 } from "react-icons/fa";
 import { getAuth } from "firebase/auth";
 
-
-
 export default function Listing() {
-    const auth = getAuth();
-    const params = useParams();
-    const [listing, setListing] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [shareLinkCopied, setShareLinkCopied] = useState(false);
-    const [contactLandlord, setContactLandlord] = useState(false);
-    SwiperCore.use([Autoplay, Navigation, Pagination]);
-    useEffect(() => {
-      async function fetchListing() {
-        const docRef = doc(db, "listings", params.listingId);
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
-          setListing(docSnap.data());
-          setLoading(false);
-        }
+  const auth = getAuth();
+  const params = useParams();
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
+  SwiperCore.use([Autoplay, Navigation, Pagination]);
+
+  useEffect(() => {
+    async function fetchListing() {
+      const docRef = doc(db, "listings", params.listingId);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setListing(docSnap.data());
+        setLoading(false);
       }
-      fetchListing();
-    }, [params.listingId]);
-    if (loading) {
-      return <Spinner />;
     }
-    return (
-      <main>
-        <Swiper
-          slidesPerView={1}
-          navigation
-          pagination={{ type: "progressbar" }}
-          effect="fade"
-          modules={[EffectFade]}
-          autoplay={{ delay: 3000 }}
-        >
-          {listing.imgUrls.map((url, index) => (
-            <SwiperSlide key={index}>
-              <div
-                className="relative w-full overflow-hidden h-[300px]"
-                style={{
-                  background: `url(${listing.imgUrls[index]}) center no-repeat`,
-                  backgroundSize: "cover",
-                }}
-              ></div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+    fetchListing();
+  }, [params.listingId]);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  const handleShareOnFacebook = () => {
+    const url = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+      window.location.href
+    )}`;
+    const options = "width=550,height=400,left=100,top=100";
+    window.open(url, "", options);
+    copyToClipboard(window.location.href);
+  };
+
+  const handleShareOnWhatsApp = () => {
+    const message = encodeURIComponent(
+      `Check out this property listing: ${window.location.href}`
+    );
+    const url = `https://api.whatsapp.com/send?text=${message}`;
+    const options = "width=550,height=400,left=100,top=100";
+    window.open(url, "", options);
+    copyToClipboard(window.location.href);
+  };
+
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text);
+    setShareLinkCopied(true);
+    setTimeout(() => {
+      setShareLinkCopied(false);
+    }, 3000);
+  };
+
+  return (
+    <main>
+      <Swiper
+        slidesPerView={1}
+        navigation
+        pagination={{ type: "progressbar" }}
+        effect="fade"
+        modules={[EffectFade]}
+        autoplay={{ delay: 3000 }}
+      >
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              className="relative w-full overflow-hidden h-[700px]"
+              style={{
+                background: `url(${listing.imgUrls[index]}) left no-repeat`,
+                backgroundSize: "100% 110%",
+              }}
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+
+     
+
+      <div className="fixed top-[13%] right-[3%] z-10 bg-transparent cursor-pointer border-2 border-transparent rounded-full w-12 h-12 flex justify-center items-center">
+      <span className="mr-4 cursor-pointer text-blue-700 mt-1 bg-white rounded-full " onClick={handleShareOnFacebook}>
+  <FaFacebook size={45} />
+</span>
+<span className="mr-4 cursor-pointer text-green-500 bg-white rounded-full " onClick={handleShareOnWhatsApp}>
+  <FaWhatsapp size={45} />
+</span>
+
+</div>
+
+
+
+
+
+
+      {shareLinkCopied && (
+        <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">
+          Link Copied
+        </p>
+      )}
         
       </main>
     );
